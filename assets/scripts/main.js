df63f97c4361d21a8e25e793ce2d63c834b09240
Damien Sakora <contactdsakora@gmail.com>
2019-06-14 23:06:47 +1000

    Created responsive hamburger menu

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
+    
     $('header div div:nth-child(2) div').click(function () {
         $(this).toggleClass('open');
+        $('nav').toggleClass('show');
+    });
+    //Navigation handle window resizing
+    $(window).resize(function(){
+        $('header div div:nth-child(2) div').removeClass('open');
+        $('nav').removeClass('show');
     });
 });
