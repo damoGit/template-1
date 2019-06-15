9a475755f864f406d23c8ce88794a725a7e873bc
Damien Sakora <contactdsakora@gmail.com>
2019-06-16 07:37:37 +1000

    Created stickey header on scroll

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,47 @@
 $(document).ready(function () {
-    
+
+    var scrolling,
+        scrollUp,
+        scrollDown;
+
+    // Navigation toggle classes
     $('header div div:nth-child(2) div').click(function () {
-        $(this).toggleClass('open');
+        $(this).toggleClass('animate');
         $('nav').toggleClass('show');
     });
-    //Navigation handle window resizing
-    $(window).resize(function(){
-        $('header div div:nth-child(2) div').removeClass('open');
+
+    // Navigation handle window resizing
+    $(window).resize(function () {
+        $('header div div:nth-child(2) div').removeClass('animate');
         $('nav').removeClass('show');
     });
+
+    //
+    $(window).scroll(function () {
+        $('header div div:nth-child(2) div').removeClass('animate');
+        $('nav').removeClass('show');
+        if (scrollUp) {
+            $('header').fadeIn('fast');
+        } else {
+            $('header').fadeOut('fast');
+        }
+    });
+
+    // Detect scroll & scroll direction
+    (function () {
+        var previousScroll = 0;
+        $(window).scroll(function () {
+            var currentScroll = $(this).scrollTop();
+            scrolling = true;
+            if (currentScroll > previousScroll) {
+                scrollUp = false;
+                scrollDown = true;
+            } else {
+                scrollUp = true;
+                scrollDown = false;
+            }
+            previousScroll = currentScroll;
+        });
+    }());
+
 });
